c577057bd60d03779b40ac59718f5e710c6d9299
DoormatIka <trashcanartistlily@gmail.com>
2023-04-10 03:12:52 +0800

    netlify and csp added

--- src/routes/reaping/+page.svelte
+++ src/routes/reaping/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  const default_image = "https://images-ext-1.discordapp.net/external/HJNf2kgLANTjmvnPUPk-VwaJFsALQDwB1wkW7UWEuUg/https/i.pinimg.com/originals/68/07/39/680739eaa243c983e7d345a23a6269d8.jpg?width=676&height=676"
+  const default_image = "placeholder.jpg"
   const players: { name: string, picture: string }[] = [
     { name: "zent", picture: default_image },
     { name: "dankmemez", picture: default_image },
