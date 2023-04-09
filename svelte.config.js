5f208625a2db337434b67cde56bb5cdee5901ac4
DoormatIka <trashcanartistlily@gmail.com>
2023-04-10 03:26:10 +0800

    split

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
       edge: true,
-      split: true
+      split: false
     }),
     csp: {
       mode: "auto",
